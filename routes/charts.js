7ce040ad4da31c70c1cae39d5bd025765ec45baf
benonimus <benonimuss@gmail.com>
2025-11-30 04:09:24 +0300

    rm

--- routes/charts.js
+++ routes/charts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const axios = require('axios');
-const ccxt = require('ccxt');
 const MarketPrice = require('../MarketPrice');
 const router = express.Router();
 
